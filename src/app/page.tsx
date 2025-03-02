c52d85257690511bdbdeffd2dad9294e300bac1a
trungls1706 <trungls1706@gmail.com>
2025-03-02 10:00:29 +0700

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,13 +20,12 @@ export default function Home() {
         initial={{ x: 100, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ duration: 0.6 }}
-        className={`flex-1 overflow-y-auto p-4 lg:p-6 order-last lg:ml-[28.57%] ${
+        className={`w-[71.43%] overflow-y-auto p-4 lg:p-6 order-last fixed right-0 h-screen ${
           showDetails ? 'hidden lg:block' : 'block'
         }`}
       >
         <div 
-        className="space-y-8 lg:space-y-12 max-w-5xl mx-auto">
-          {/* <Projects /> */}
+        className="space-y-8 lg:space-y-12 w-full">
           <Experience />
         </div>
       </motion.div>
